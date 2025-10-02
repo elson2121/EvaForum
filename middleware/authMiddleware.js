98a4694c79b792c56b7cd1618aeeb268c2775989
elson2121 <elsonsd21@gmail.com>
2025-10-02 16:45:31 -0300

    some file changes and some new added code added to the file

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,9 +11,13 @@ const jwt = require('jsonwebtoken')
     console.log(authHeader)
     console.log(token)
     try {
-        const {username,userid}= jwt.verify(authHeader,"secret");
-        req.user={username,userid}
-      next()
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || process.env.JWT_SECRET); // Use the secret, not the token
+        req.user = { username: decoded.username, userid: decoded.userid }; // Adjust based on payload
+        next();
+      //   const {username,userid}= jwt.verify(authHeader,token);
+      //   req.user={username,userid}
+      // next()
     } catch (error) {
          return res.status(StatusCodes.UNAUTHORIZED).json({error:"Authentication invalid"});
 }
